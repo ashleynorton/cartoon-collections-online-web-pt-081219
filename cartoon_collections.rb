eec936ebf1a86a1ce3a8e271c8a5cf8f82970926
Ashley Norton <nortonashleym@gmail.com>
2019-09-04 21:54:59 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,5 +19,5 @@ def find_the_cheese (array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   
-  array.detect {|i| i.include?
+  array.detect {|i| cheese_types.include? i}
 end
